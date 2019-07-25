616532e4b009ee8559dcea33315c83de5a43cbaa
yangyingsa <2714326502@qq.com>
2019-07-25 21:00:39 +0800

    第二次上传

---
+++ day2/src/com/bw/action/StuAction.java
@@ -0,0 +1,14 @@
+package com.bw.action;
+import java.util.Arrays;
+import java.util.Random;
+
+public class StuAction {
+	public static void main(String[] args) {
+		int arr[]=new int[20];
+		Random random=new Random();
+		for(int i=0;i<=0;i++) {
+			random.nextInt();
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}
