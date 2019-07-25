e491a03266dc8d03c49595983ce348a8046dbce6
yangyingsa <2714326502@qq.com>
2019-07-25 21:14:01 +0800

    一个项目

---
+++ demo/src/main/java/demo/Text.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Text{
+public static void main(String[] args) {
+	String a="aaaa";
+	a=a.replaceAll("a","aaaaa");
+	a=a.replaceAll("a","aaaaa");
+	System.out.println(a+a.length());
+	
+}
+
+}
+
+
